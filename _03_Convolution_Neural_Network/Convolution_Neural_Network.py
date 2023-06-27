5316f300d46bdf7f5ea42ce3b9d558296d82a8a4
siwenwei <siwenwei@hhu.edu.cn>
2023-06-27 13:04:00 +0800

    first

--- _03_Convolution_Neural_Network/Convolution_Neural_Network.py
+++ _03_Convolution_Neural_Network/Convolution_Neural_Network.py
@@ -1,10 +1,10 @@
-# 在该文件NeuralNetwork类中定义你的模型 
+# 在该文件NeuralNetwork类中定义你的模型
 # 在自己电脑上训练好模型，保存参数，在这里读取模型参数（不要使用JIT读取），在main中返回读取了模型参数的模型
 
 import os
 
-os.system("sudo pip3 install torch")
-os.system("sudo pip3 install torchvision")
+# os.system("sudo pip3 install torch")
+# os.system("sudo pip3 install torchvision")
 
 import torch
 import torch.nn as nn
@@ -12,13 +12,14 @@ import torch.optim as optim
 import torchvision
 
 from torch.utils.data import DataLoader
-    
+
 if torch.cuda.is_available():
     device = torch.device("cuda:0")
     print("使用GPU进行训练")
 else:
     device = torch.device("cpu")
     print("使用cpu进行训练")
+
 
 # todo Bottleneck
 class Bottleneck(nn.Module):
@@ -154,11 +155,14 @@ def resnet50(num_classes=10):
 def read_data():
     # 这里可自行修改数据预处理，batch大小也可自行调整
     # 保持本地训练的数据读取和这里一致
-    dataset_train = torchvision.datasets.CIFAR10(root='../data/exp03', train=True, download=True, transform=torchvision.transforms.ToTensor())
-    dataset_val = torchvision.datasets.CIFAR10(root='../data/exp03', train=False, download=False, transform=torchvision.transforms.ToTensor())
+    dataset_train = torchvision.datasets.CIFAR10(root='../data/exp03', train=True, download=True,
+                                                 transform=torchvision.transforms.ToTensor())
+    dataset_val = torchvision.datasets.CIFAR10(root='../data/exp03', train=False, download=False,
+                                               transform=torchvision.transforms.ToTensor())
     data_loader_train = DataLoader(dataset=dataset_train, batch_size=256, shuffle=True)
     data_loader_val = DataLoader(dataset=dataset_val, batch_size=256, shuffle=False)
     return dataset_train, dataset_val, data_loader_train, data_loader_val
+
 
 def main():
     model = resnet50()  # 若有参数则传入
@@ -166,4 +170,3 @@ def main():
     parent_dir = os.path.dirname(current_dir)
     model.load_state_dict(torch.load(parent_dir + '/pth/model.pth', map_location='cpu'))
     return model
-    
